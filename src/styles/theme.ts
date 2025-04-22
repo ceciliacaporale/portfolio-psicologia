47d1095b139d18873427584e25edb37b4f9f6417
ceciliacaporale <cecicaporale16@gmail.com>
2025-04-22 20:12:50 -0300

    [feat] - criando theme inicial e global css

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+        gray: '#EEE2DF',
+        beige: '#F0DCCA',
+        brown: '#705D56',
+        orange: '#C97C5D',
+        terracotta: '#B36A5E'
+    }
+}
