91c4c4dfa091fb3c856d9c4214e4920d04c34b86
ceciliacaporale <cecicaporale16@gmail.com>
2025-04-27 20:36:31 -0300

    feat: novo componente divider

---
+++ src/components/Divider/Divider.styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  z-index: 99;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 350.43px;
+  height: 0;
+  text-align: center;
+  position: relative;
+
+  .transparent-button {
+    margin: 0 auto;
+    background: none;
+    border: none;
+    cursor: pointer;
+    height: 45px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const DividerSvg = styled.svg`
+  width: 100%;
+  height: auto;
+  display: block;
+  position: relative;
+`;
