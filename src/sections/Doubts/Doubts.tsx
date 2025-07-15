98299909b58d447d04dd0e0a2e3c31ef6866dd78
Maria Cecília <cecicaporale16@gmail.com>
2025-07-15 15:01:23 -0300

    feat: add novos dois conteudos de duvidas

--- src/sections/Doubts/Doubts.tsx
+++ src/sections/Doubts/Doubts.tsx
@@ -15,6 +15,14 @@ const duvidas = [
     title: "Qual é a frequência e duração das sessões?",
     content: "As sessões são semanais com duração de 50 minutos. A duração do processo terapêutico varia de pessoa para pessoa, a depender da demanda e do progresso.  Todo o processo é acompanhado e ajustado conforme necessário.",
   },
+  {
+    title: "O que é a TCC?",
+    content:"A Terapia Cognitivo-Comportamental (TCC) é uma abordagem da psicologia que entende que nossos pensamentos, emoções e comportamentos estão interligados. O processo terapêutico é baseado na identificação de pensamentos disfuncionais, suas crenças e padrões de comportamento que estão atuando na manutenção do sofrimento. Assim, o paciente será guiado para aprender a desenvolver, de forma colaborativa com o terapeuta, formas mais equilibradas de pensar e se comportar diante das situações do dia a dia. A TCC é estruturada, focada no presente e voltada para a resolução de problemas, sendo eficaz para ansiedade, depressão, fobias, entre outros. Além disso, monitora continuamente o progresso do paciente, tendo como objetivo a promoção de qualidade de vida e autonomia.",
+  },
+  {
+    title: "Qual o valor das sessões?",
+    content: "O serviço é prestado em forma de assinatura mensal que inclui 4 sessões de psicoterapia semanais em horário fixo, com duração de 50 minutos. Para consultar os valores entre em contato.",
+  },
 ];
 
 const Doubts = () => (
