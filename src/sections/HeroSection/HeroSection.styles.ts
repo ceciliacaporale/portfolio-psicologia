7caa50784a028b25863a19dc75cc8f92fa2ff14f
Maria Cecília <cecicaporale16@gmail.com>
2025-09-14 22:43:06 -0300

    feat: add hh1 hidden para buscas google

--- src/sections/HeroSection/HeroSection.styles.ts
+++ src/sections/HeroSection/HeroSection.styles.ts
@@ -54,7 +54,19 @@ export const ContainerText = styled.div`
     padding: 0;
   }
 
-  h1 {
+  .visually-hidden {
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+  }
+  
+  h2 {
     font-size: 3rem;
     color: #705D56;
     font-family: 'Mansory', sans-serif;
